78f66eaacfe791225b426c1451a0dbc461178255
GabrielDertoni <gab.dertoni@usp.br>
2020-12-01 19:42:26 -0300

    Aditional documentation and bugfix in Skip List

--- includes/skip_list.h
+++ includes/skip_list.h
@@ -2,8 +2,19 @@
  * Author: Gabriel Dertoni
  * Github: https://github.com/GabrielDertoni
  *
- * This implementation defines a linked skip list wich means the layers are also
- * linked together.
+ * This implementation defines a linked skip list wich means that the layers are
+ * also linked together.
+ * This documentation uses an annotation for parameters and return types that is
+ * related to the Rust Programming Language. In short it specifies who is 
+ * responsible for a given pointer meaning which function shuld free its content.
+ * In this context, [ref] means the value is only a reference and does not imply
+ * any sort of responsability transfer. If a function receives a [ref] annotated
+ * argument, it will never free its contents nor will it change it in any way.
+ * Similarly if a function returns a [ref] the return value should not be freed.
+ * By contrast, a [ownership] means that the receiving end can change values as
+ * it likes but is also responsible for freeing the contents of the pointer.
+ * Finally, the [mut ref] means that the receiving end may change the contents of
+ * the pointer, but it may not free it.
  */
 
 
@@ -12,12 +23,11 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include <dict_type.h>
 
-#define ELEM_MIN strdup("")
-#define ELEM_MAX "ZZZZZZZZZZ"
-
-// The general element type. This may be changed and recompiled to whichever type.
-typedef char * elem_t;
+// The general element type. This may be changed and recompiled to whichever type
+// is appropriate.
+typedef dict_entry_t * elem_t;
 
 // The type used to reference a node in the skip list.
 typedef struct _skip_node skip_node_t;
@@ -30,75 +40,10 @@ typedef void (*free_fn_t)(elem_t);
 
 // A type for functions that can compare two elem_t and return an integer > 0 if
 // the first one is bigger, < 0 if the second one is bigger and 0 if they are equal.
+// This function should have the property that if one of the elements is the elem_min
+// provided in skip_list_create, the function should always return that elem_min is
+// smaller than eny other element.
 typedef int (*comp_fn_t)(elem_t, elem_t);
-
-/* ======================== SKIP NODE ======================== */
-
-/**
- * Creates a new skip node.
- *
- * @param elem - the element that will be stored in the skip node. [ownership]
- * @param next - a pointer to the next node in the skip list. [ref]
- * @param below - a pointer to a node below this one in the skip list. [ref]
- * @return the newly created (allocated) node. [ownership]
- */
-skip_node_t *skip_node_create(elem_t elem, skip_node_t *next, skip_node_t *below);
-
-/* GETTERS */
-
-/**
- * Gets the next node in the skip list.
- *
- * @param node - the node that will be used as reference to get the next. [ref]
- * @return the next node. [ref]
- */
-skip_node_t *skip_node_next(skip_node_t *node);
-
-/**
- * Gets the node below in the skip list.
- *
- * @param node - the node that will be used as reference to get the below. [ref]
- * @return the below node. [ref]
- */
-skip_node_t *skip_node_below(skip_node_t *node);
-
-/**
- * Gets the value inside the node.
- *
- * @param node - the node to get the value inside. [ref]
- * @return the value inside the node. [ref].
- */
-elem_t skip_node_value(skip_node_t *node);
-
-/* METHODS */
-
-/**
- * Destroys a node and returns value inside.
- *
- * @param node - the node to be freed. [ownership]
- * @return the value inside of the node. [ownership].
- */
-elem_t skip_node_destroy(skip_node_t *node);
-
-/**
- * Recursively insert an element in the skip list.
- *
- * @param head - the node to be used as a head (will always insert after and never on top of it). [mut ref]
- * @param elem - the element value to be inserted. [ownership]
- * @param comp - the comparison function between two elements. [ref]
- * @return if a node was inserted in the same layer as head, returns the node, else returns NULL. [ref]
- */
-skip_node_t *skip_node_insert(skip_node_t *head, elem_t elem, comp_fn_t comp);
-
-/**
- * Recursively remove the first elment that is equal to elem in the skip list.
- *
- * @param head - the node to be used as a head (will always remove after and never on top of it). [mut ref]
- * @param elem - the element to be searched through the list, the first that is found is removed. [ref]
- * @param comp_fn_t - the comparison function between two elements. [ref]
- * @return a linked list of all removed nodes (using skip_node_t but all of them have below = NULL). [ownership]
- */
-skip_node_t *skip_node_remove(skip_node_t *head, elem_t elem, comp_fn_t comp);
 
 /* ============ SKIP LIST ============ */
 
@@ -109,7 +54,7 @@ skip_node_t *skip_node_remove(skip_node_t *head, elem_t elem, comp_fn_t comp);
  * @param comp_fn_t - the comparison function between two elements. [ref]
  * @return the newly created (allocated) skip list. [ownership]
  */
-skip_list_t *skip_list_create(uint layer_max, comp_fn_t comp);
+skip_list_t *skip_list_create(comp_fn_t comp, elem_t elem_min, uint layer_max);
 
 /* GETTERS */
 
@@ -159,12 +104,22 @@ int skip_list_compare(skip_list_t *l, skip_node_t *a, skip_node_t *b);
 skip_node_t *skip_list_find(skip_list_t *l, elem_t elem);
 
 /**
+ * The first occurrence of elem in the skip list searched with some compare function.
+ *
+ * @param l - the skip list. [ref]
+ * @param elem - the element to be found.
+ * @param comp - the function to use while searching.
+ * @return NULL if the element is not found, a reference to the topmost node to contain elem. [ref]
+ */
+skip_node_t *skip_list_find_with(skip_list_t *l, elem_t elem, comp_fn_t comp);
+
+/**
  * Inserts an element in the skip list.
  *
  * @param l - the skip list. [mut ref]
  * @param elem - the element to be inserted. [ownership]
  */
-void skip_list_insert(skip_list_t *l, elem_t elem);
+bool skip_list_insert(skip_list_t *l, elem_t elem);
 
 /**
  * Removes the first occurrence of elem in the skip list.
@@ -193,11 +148,81 @@ bool skip_list_remove_free(skip_list_t *l, elem_t elem, free_fn_t free_func);
  */
 void skip_list_destroy(skip_list_t *l, free_fn_t free_func);
 
+/* ======================== SKIP NODE ======================== */
 
-/* ============ HELPERS ============ */
+/**
+ * Creates a new skip node.
+ *
+ * @param elem - the element that will be stored in the skip node. [ownership]
+ * @param next - a pointer to the next node in the skip list. [ref]
+ * @param below - a pointer to a node below this one in the skip list. [ref]
+ * @return the newly created (allocated) node. [ownership]
+ */
+skip_node_t *skip_node_create(elem_t elem, skip_node_t *next, skip_node_t *below);
 
-// void stack_free(elem_t elem);
-void heap_free(elem_t elem);
-int compare(elem_t a, elem_t b);
+/* GETTERS */
+
+/**
+ * Gets the next node in the skip list.
+ *
+ * @param node - the node that will be used as reference to get the next. [ref]
+ * @return the next node. [ref]
+ */
+skip_node_t *skip_node_next(skip_node_t *node);
+
+/**
+ * Gets the node below in the skip list.
+ *
+ * @param node - the node that will be used as reference to get the below. [ref]
+ * @return the below node. [ref]
+ */
+skip_node_t *skip_node_below(skip_node_t *node);
+
+/**
+ * Gets the value inside the node.
+ *
+ * @param node - the node to get the value inside. [ref]
+ * @return the value inside the node. [ref].
+ */
+elem_t skip_node_value(skip_node_t *node);
+
+/* SETTERS */
+
+/**
+ * Sets the value inside the node.
+ *
+ * @param node - the node to set the value inside. [ref]
+ */
+void skip_node_set(skip_node_t *node, elem_t new_val);
+
+/* METHODS */
+
+/**
+ * Destroys a node and returns value inside.
+ *
+ * @param node - the node to be freed. [ownership]
+ * @return the value inside of the node. [ownership].
+ */
+elem_t skip_node_destroy(skip_node_t *node);
+
+/**
+ * Recursively insert an element in the skip list.
+ *
+ * @param head - the node to be used as a head (will always insert after and never on top of it). [mut ref]
+ * @param elem - the element value to be inserted. [ownership]
+ * @param comp - the comparison function between two elements. [ref]
+ * @return if a node was inserted in the same layer as head, returns the node, else returns NULL. [ref]
+ */
+skip_node_t *skip_node_insert(skip_node_t *head, elem_t elem, comp_fn_t comp);
+
+/**
+ * Recursively remove the first elment that is equal to elem in the skip list.
+ *
+ * @param head - the node to be used as a head (will always remove after and never on top of it). [mut ref]
+ * @param elem - the element to be searched through the list, the first that is found is removed. [ref]
+ * @param comp_fn_t - the comparison function between two elements. [ref]
+ * @return a linked list of all removed nodes (using skip_node_t but all of them have below = NULL). [ownership]
+ */
+skip_node_t *skip_node_remove(skip_node_t *head, elem_t elem, comp_fn_t comp);
 
 #endif
--- src/skip_list.c
+++ src/skip_list.c
@@ -21,67 +21,13 @@ struct _skip_list {
     uint height;
 };
 
-/* ============ SKIP NODE ============ */
-
-skip_node_t *skip_node_create(elem_t elem, skip_node_t *next, skip_node_t *below) {
-    skip_node_t *node = (skip_node_t *)malloc(sizeof(skip_node_t));
-    node->value = elem;
-    node->next = next;
-    node->below = below;
-    return node;
-}
-
-/* GETTERS */
-
-skip_node_t *skip_node_next(skip_node_t *node) { return node->next; }
-skip_node_t *skip_node_below(skip_node_t *node) { return node->below; }
-elem_t skip_node_value(skip_node_t *node) { return node->value; }
-
-/* METHODS */
-
-skip_node_t *skip_node_insert(skip_node_t *head, elem_t elem, comp_fn_t comp) {
-    skip_node_t *ptr;
-    for (ptr = head; ptr->next && comp(ptr->next->value, elem) <= 0; ptr = ptr->next);
-
-    skip_node_t *below = NULL;
-    // If there is a node below and a node has not been inserted below and
-    // the coin flip failed, do not add a node to this layer.
-    if (ptr->below && !((below = skip_node_insert(ptr->below, elem, comp)) && rand() % 2))
-        return NULL;
-
-    return ptr->next = skip_node_create(elem, ptr->next, below);
-}
-
-// Will return a node forms a linked list pointers.
-skip_node_t *skip_node_remove(skip_node_t *head, elem_t elem, comp_fn_t comp) {
-    skip_node_t *ptr, *removed = NULL;
-    for (ptr = head; ptr->next && comp(ptr->next->value, elem) < 0; ptr = ptr->next);
-
-    if (ptr->below)
-        removed = skip_node_remove(ptr->below, elem, comp);
-
-    if (ptr->next && comp(ptr->next->value, elem) == 0) {
-        removed = ptr->next;
-        ptr->next = removed->next;
-        removed->next = removed->below;
-        removed->below = NULL;
-    }
-
-    return removed;
-}
-
-elem_t skip_node_destroy(skip_node_t *node) {
-    elem_t elem = node->value;
-    free(node);
-    return elem;
-}
-
 /* ============ SKIP LIST METHODS ============ */
 
-skip_list_t *skip_list_create(uint layer_max, comp_fn_t comp) {
-    srand(time(NULL));
+skip_list_t *skip_list_create(comp_fn_t comp, elem_t elem_min, uint layer_max) {
+    // srand(time(NULL));
+    srand(53);
     skip_list_t *skip_list = (skip_list_t *)malloc(sizeof(skip_list_t));
-    skip_list->head = skip_node_create(ELEM_MIN, NULL, NULL);
+    skip_list->head = skip_node_create(elem_min, NULL, NULL);
     skip_list->comp = comp;
     skip_list->size = 0;
     skip_list->layer_max = layer_max;
@@ -98,16 +44,26 @@ uint skip_list_size(skip_list_t *l) { return l->size; }
 /* METHODS */
 
 skip_node_t *skip_list_find(skip_list_t *l, elem_t elem) {
-    int cmp;
-    skip_node_t *ptr;
-    for (ptr = l->head; ptr->below && cmp; ptr = ptr->below)
-        for (; ptr->next && (cmp = l->comp(ptr->next->value, elem)) < 0; ptr = ptr->next);
+    return skip_list_find_with(l, elem, l->comp);
+}
 
-    if (!ptr->next || l->comp(ptr->next->value, elem) != 0) return NULL;
+skip_node_t *skip_list_find_with(skip_list_t *l, elem_t elem, comp_fn_t comp) {
+    if (!l->head) return NULL;
+
+    skip_node_t *ptr;
+    for (ptr = l->head; ptr->below; ptr = ptr->below)
+        for (; ptr->next && comp(ptr->next->value, elem) < 0; ptr = ptr->next);
+
+    for (; ptr->next && comp(ptr->next->value, elem) < 0; ptr = ptr->next);
+
+    if (!ptr->next || comp(ptr->next->value, elem) != 0) return NULL;
     return ptr->next;
 }
 
-void skip_list_insert(skip_list_t *l, elem_t elem) {
+bool skip_list_insert(skip_list_t *l, elem_t elem) {
+    skip_node_t *result = skip_list_find(l, elem);
+    if (result) return false;
+
     skip_node_t *below;
     if ((below = skip_node_insert(l->head, elem, l->comp)) && rand() % 2 && l->height < l->layer_max) {
         skip_node_t *top = skip_node_create(elem, NULL, below);
@@ -116,7 +72,7 @@ void skip_list_insert(skip_list_t *l, elem_t elem) {
     }
 
     l->size++;
-    return;
+    return true;
 }
 
 skip_node_t *skip_list_remove(skip_list_t *l, elem_t elem) {
@@ -146,6 +102,7 @@ bool skip_list_remove_free(skip_list_t *l, elem_t elem, free_fn_t free_func) {
 void skip_list_destroy(skip_list_t *l, free_fn_t free_func) {
     for (skip_node_t *layer = l->head; layer;) {
         skip_node_t *layer_below = layer->below;
+
         for (skip_node_t *ptr = layer; ptr;) {
             skip_node_t *ptr_next = ptr->next;
             elem_t elem = skip_node_destroy(ptr);
@@ -161,30 +118,79 @@ void skip_list_destroy(skip_list_t *l, free_fn_t free_func) {
     free(l);
 }
 
-int skip_list_compare(skip_list_t *l, skip_node_t *a, skip_node_t *b) {
-    return l->comp(a->value, b->value);
+
+/* ============ SKIP NODE ============ */
+
+skip_node_t *skip_node_create(elem_t elem, skip_node_t *next, skip_node_t *below) {
+    skip_node_t *node = (skip_node_t *)malloc(sizeof(skip_node_t));
+    node->value = elem;
+    node->next = next;
+    node->below = below;
+    return node;
 }
 
-/* ============ HELPERS ============ */
+/* GETTERS */
 
-void heap_free(elem_t elem) {
-    free(elem);
+skip_node_t *skip_node_next(skip_node_t *node) { return node->next; }
+skip_node_t *skip_node_below(skip_node_t *node) { return node->below; }
+elem_t skip_node_value(skip_node_t *node) { return node->value; }
+
+/* SETTERS */
+
+void skip_node_set(skip_node_t *node, elem_t new_val) { node->value = new_val; }
+
+/* METHODS */
+
+skip_node_t *skip_node_insert(skip_node_t *head, elem_t elem, comp_fn_t comp) {
+    skip_node_t *ptr;
+    for (ptr = head; ptr->next && comp(ptr->next->value, elem) <= 0; ptr = ptr->next);
+
+    skip_node_t *below = NULL;
+    // If there is a node below and a node has not been inserted below and
+    // the coin flip failed, do not add a node to this layer.
+    if (ptr->below && !((below = skip_node_insert(ptr->below, elem, comp)) && rand() % 2))
+        return NULL;
+
+    return ptr->next = skip_node_create(elem, ptr->next, below);
 }
 
-int compare(elem_t a, elem_t b) {
-    return strcmp(a, b);
+// Will return a node that forms a linked list of pointers.
+skip_node_t *skip_node_remove(skip_node_t *head, elem_t elem, comp_fn_t comp) {
+    skip_node_t *ptr = head, *removed = NULL, *last_removed = NULL;
+
+    for (ptr = head; ptr; ptr = ptr->below) {
+        for (; ptr->next && comp(ptr->next->value, elem) < 0; ptr = ptr->next);
+        if (ptr->next && comp(ptr->next->value, elem) == 0) {
+            last_removed = ptr->next;
+            ptr->next = last_removed->next;
+            last_removed->next = last_removed->below;
+            last_removed->below = NULL;
+
+            if (!removed) removed = last_removed;
+        }
+    }
+
+    return removed;
+    /*
+    skip_node_t *ptr, *removed = NULL;
+    for (ptr = head; ptr->next && comp(ptr->next->value, elem) < 0; ptr = ptr->next);
+
+    if (ptr->below)
+        removed = skip_node_remove(ptr->below, elem, comp);
+
+    if (ptr->next && comp(ptr->next->value, elem) == 0) {
+        removed = ptr->next;
+        ptr->next = removed->next;
+        removed->next = removed->below;
+        removed->below = NULL;
+    }
+
+    return removed;
+    */
 }
 
-/*
-void stack_free(elem_t elem) {
-    return;
+elem_t skip_node_destroy(skip_node_t *node) {
+    elem_t elem = node->value;
+    free(node);
+    return elem;
 }
-
-int compare(elem_t a, elem_t b) {
-    return a > b ? 1 : (a < b ? -1 : 0);
-}
-*/
-
-
-
-
